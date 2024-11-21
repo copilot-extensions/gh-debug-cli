7481688a505af5b580952c79e29d784bf289964e
calvinmvrk <calvinmvrk@github.com>
2024-11-21 14:05:45 -0600

    update help logic

--- cmd/rootCmd.go
+++ cmd/rootCmd.go
@@ -9,11 +9,10 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "gh-debug-cli",
 	Short: "A CLI tool for debugging",
 	Long:  `This CLI tool allows you to debug your agent by chatting with it locally.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Use 'gh-debug-cli --help' to see available commands")
+		// fmt.Println("Use 'gh debug-cli --help' to see available commands")
 	},
 }
 
